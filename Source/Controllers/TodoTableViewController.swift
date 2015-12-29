91fe5ac88a2bdaebb10fc7bd3a0a7ddbc10cc36a
Jake Craige <james.craige@gmail.com>
2015-12-29 15:07:58 -0800

    Organize TodoTableView into sections based on functionality

--- Source/Controllers/TodoTableViewController.swift
+++ Source/Controllers/TodoTableViewController.swift
@@ -12,7 +12,30 @@ class TodoTableViewController: UITableViewController {
             self.viewModel = TodosViewModel(todos: todos)
         }
     }
+}
 
+// MARK: Actions
+extension TodoTableViewController {
+    @IBAction func addTapped(sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Create", message: "Create a new todo item", preferredStyle: .Alert)
+
+        alertController.addTextFieldWithConfigurationHandler() { textField in
+            textField.placeholder = "Name"
+        }
+
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .Cancel) { _ in })
+        alertController.addAction(UIAlertAction(title: "Create", style: .Default) { _ in
+            guard let name = alertController.textFields?.first?.text else { return }
+
+            store.dispatch(CreateTodoAction(name: name))
+        })
+
+        presentViewController(alertController, animated: false, completion: nil)
+    }
+}
+
+// MARK: UITableViewController
+extension TodoTableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.todos.count
     }
@@ -37,22 +60,5 @@ class TodoTableViewController: UITableViewController {
             let todo = viewModel.todoForIndexPath(indexPath)
             store.dispatch(DeleteTodoAction(todo: todo))
         }
-    }
-
-    @IBAction func addTapped(sender: UIBarButtonItem) {
-        let alertController = UIAlertController(title: "Create", message: "Create a new todo item", preferredStyle: .Alert)
-
-        alertController.addTextFieldWithConfigurationHandler() { textField in
-            textField.placeholder = "Name"
-        }
-
-        alertController.addAction(UIAlertAction(title: "Cancel", style: .Cancel) { _ in })
-        alertController.addAction(UIAlertAction(title: "Create", style: .Default) { _ in
-            guard let name = alertController.textFields?.first?.text else { return }
-
-            store.dispatch(CreateTodoAction(name: name))
-        })
-
-        presentViewController(alertController, animated: false, completion: nil)
     }
 }
