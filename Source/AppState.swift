57bf08ef83b928ea5a7263808da4660950b31033
Jake Craige <james.craige@gmail.com>
2015-12-29 15:52:33 -0800

    Increase amount of initial todos

--- Source/AppState.swift
+++ Source/AppState.swift
@@ -1,10 +1,15 @@
 import ReactiveCocoa
 
-struct AppState {
-    let todos = MutableProperty([
-        Todo(name: "Create example app", completed: true),
-        Todo(name: "Celebrate", completed: false),
-    ])
+private let initialTodos = [
+    Todo(name: "Have tea with the Queen", completed: true),
+    Todo(name: "Circumnavigate the globe", completed: false),
+    Todo(name: "Race a cheetah", completed: false),
+    Todo(name: "Climb Mount Everest", completed: true),
+    Todo(name: "Jump out of a plane", completed: false),
+    Todo(name: "Read a book", completed: false),
+]
 
+struct AppState {
+    let todos = MutableProperty(initialTodos)
     let filter = MutableProperty(TodoFilter.All)
 }
