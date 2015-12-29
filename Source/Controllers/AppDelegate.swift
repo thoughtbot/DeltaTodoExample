2300cc8d072cc0fa7028ac8d4261e6a87bce7cfa
Jake Craige <james.craige@gmail.com>
2015-12-29 10:09:59 -0800

    Initial Commit

---
+++ Source/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  DeltaTodoExample
+//
+//  Created by James Craige on 12/29/15
+//  Copyright (c) 2015 thoughtbot. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
